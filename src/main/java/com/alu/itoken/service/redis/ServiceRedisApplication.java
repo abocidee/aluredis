453d174b18bb208b385b06a8365357cef5c7bc43
770190990@qq.com <770190990@qq.com>
2022-01-16 23:11:47 +0800

    fd

--- src/main/java/com/alu/itoken/service/redis/ServiceRedisApplication.java
+++ src/main/java/com/alu/itoken/service/redis/ServiceRedisApplication.java
@@ -2,7 +2,6 @@ package com.alu.itoken.service.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication()
